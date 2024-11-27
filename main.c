e8e4644a8444f7f049031c6758c607231b8122da
Blaeste <erwin.schwartz.pro@gmail.com>
2024-11-27 12:18:09 +0100

    main with bonus

---
+++ main.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eschwart <eschwart@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/27 11:09:22 by eschwart          #+#    #+#             */
+/*   Updated: 2024/11/27 12:06:27 by eschwart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+
+int	main(void)
+{
+	int		fd;
+	char	*next_line;
+
+	fd = open("test/empty.txt", O_RDONLY);
+	printf("%s\n", get_next_line(fd));
+	close(fd);
+	return (0);
+}
+
+/* BONUS
+int	main(void)
+{
+	char	*line;
+	int		i;
+	int		fd1;
+	int		fd2;
+	int		fd3;
+
+	fd1 = open("test/1char.txt", O_RDONLY);
+	fd2 = open("test/random.txt", O_RDONLY);
+	fd3 = open("test/text.txt", O_RDONLY);
+	i = 1;
+	while (i < 7)
+	{
+		line = get_next_line(fd1);
+		printf("line [%02d]: %s", i, line);
+		free(line);
+		line = get_next_line(fd2);
+		printf("line [%02d]: %s", i, line);
+		free(line);
+		line = get_next_line(fd3);
+		printf("line [%02d]: %s", i, line);
+		free(line);
+		i++;
+	}
+	close(fd1);
+	close(fd2);
+	close(fd3);
+	return (0);
+}*/
