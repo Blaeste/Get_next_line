d8ea373786365fbd1c9f0fccc46ebd977250b3e7
Blaeste <erwin.schwartz.pro@gmail.com>
2024-11-27 11:11:42 +0100

    gnl static [...]

---
+++ get_next_line_utils.c
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eschwart <eschwart@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/26 10:28:18 by eschwart          #+#    #+#             */
+/*   Updated: 2024/11/27 11:06:35 by eschwart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	total_l;
+	size_t	i;
+	size_t	j;
+	char	*joined;
+
+	if (!s1 || !s2)
+		return (0);
+	total_l = ft_strlen(s1) + ft_strlen(s2);
+	joined = (char *)malloc(sizeof(char) * (total_l + 1));
+	if (!joined)
+		return (0);
+	i = 0;
+	while (s1[i])
+	{
+		joined[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		joined[i + j] = s2[j];
+		j++;
+	}
+	joined[i + j] = '\0';
+	return (joined);
+}
+
+char	*ft_strdup(const char *s)
+{
+	size_t	i;
+	size_t	len;
+	char	*dest;
+
+	len = ft_strlen(s);
+	dest = (char *)malloc((len + 1) * sizeof(char));
+	if (!dest)
+		return (0);
+	i = 0;
+	while (i < len)
+	{
+		dest[i] = s[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	unsigned char	tofind;
+
+	tofind = (unsigned char)c;
+	while (*s)
+	{
+		if (*s == tofind)
+			return ((char *)s);
+		s++;
+	}
+	if (tofind == '\0')
+		return ((char *)s);
+	return (0);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+
+	if (!s)
+		return (0);
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	if (len > ft_strlen(s) - start)
+		len = ft_strlen(s) - start;
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (0);
+	i = 0;
+	while (s[start + i] && i < len)
+	{
+		str[i] = s[start + i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
